6412423cf22ff863d3bdf4013959da7cefc3d257
miebale123 <miebalen@gmail.com>
2025-09-21 09:50:43 +0300

    chores: removed vscode setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+
 import { configuration, typeOrmConfig } from './config';
 import { TasksModule } from './tasks';
 import { AuthModule } from './auth';
