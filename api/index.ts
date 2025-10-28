befd6cb0f56aa1b25f1dfe7117106c3d5f893a10
miebale123 <miebalen@gmail.com>
2025-10-28 05:03:38 +0300

    added docs rule

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../src/app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import type { Request, Response } from 'express';
-import express from 'express';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from '../src/app.module';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import type { Request, Response } from 'express';
+// import express from 'express';
 
-const server = express();
+// const server = express();
 
-export default async function handler(req: Request, res: Response) {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  await app.init();
-  server(req, res);
+// export default async function handler(req: Request, res: Response) {
+//   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+//   await app.init();
+//   server(req, res);
+// }
+
+
+export function GET(request: Request) {
+  return new Response('Hello from Vercel!');
 }
